d8a59224ebce8e7303cf8c2be6671d5364392433
hne-hub <82113191+hne-hub@users.noreply.github.com>
2021-07-29 17:24:27 +0200

    Add files via upload
    Dice Game file js Version 2.1

--- dicegame.js
+++ dicegame.js
@@ -37,18 +37,6 @@ document.querySelector('.btn-hold').addEventListener('click', function() {
 });
 
 
-function nextPlayer() {
-    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
-    roundScore = 0;
-
-    document.getElementById('current-0').textContent = '0';
-    document.getElementById('current-1').textContent = '0';
-    document.querySelector('.player-0-panel').classList.toggle('active');
-    document.querySelector('.player-1-panel').classList.toggle('active');
-    document.querySelector('.dice').style.display = 'none';
-}
-
-
 document.querySelector('.btn-new').addEventListener('click', init);
 function init() {
     score = [0, 0];
@@ -69,4 +57,16 @@ function init() {
     document.querySelector('.player-0-panel').classList.remove('active');
     document.querySelector('.player-1-panel').classList.remove('active');
     document.querySelector('.player-0-panel').classList.add('active');
+}
+
+
+function nextPlayer() {
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+    roundScore = 0;
+
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
+    document.querySelector('.dice').style.display = 'none';
 }
